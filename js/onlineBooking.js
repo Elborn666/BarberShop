3bc11397774920881c512f612e6bea1b92f7adc8
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-07-05 09:35:06 +0300

    виправив burger menu

---
+++ js/onlineBooking.js
@@ -0,0 +1,31 @@
+const body = document.querySelector('body')
+const backdrop = document.querySelector('.backdrop__booking')
+const submitForm = document.querySelector('.modal__booking-button')
+
+const buttonsOpenModal = document.querySelectorAll('.js-booking')
+const buttonCloseModal = document.querySelector('.modal__booking-close')
+
+buttonCloseModal.addEventListener('click', onBtnClose)
+submitForm.addEventListener('submit', onSubmitForm)
+
+for (let buttonOpenModal of buttonsOpenModal) {
+    buttonOpenModal.addEventListener('click', onBtnOpenModal);
+  }
+
+function onSubmitForm (e) {
+    e.preventDefault();
+    e.currentTarget.reset()
+
+    backdrop.classList.remove('is-open')
+    body.classList.remove('hidden')
+}
+
+function onBtnOpenModal () {
+    backdrop.classList.add('is-open')
+    body.classList.add('hidden')
+}
+
+function onBtnClose () {
+    backdrop.classList.remove('is-open')
+    body.classList.remove('hidden')
+}
