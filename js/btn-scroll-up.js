d47ae35e4c557a37b334ce965c4e715703b82625
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-07-06 00:28:33 +0300

    тест скрола вгору

---
+++ js/btn-scroll-up.js
@@ -0,0 +1,13 @@
+const btnScrollUp = document.querySelector('.scroll-up__button')
+
+btnScrollUp.addEventListener('click', onBtnScrollUp)
+
+function onBtnScrollUp() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// function topFunction() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
