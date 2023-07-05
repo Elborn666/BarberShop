9b746c092e93e0d411adddfd37f74a3422406f70
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-07-06 00:35:56 +0300

    js код для анімації під час скролу

--- js/btn-scroll-up.js
+++ js/btn-scroll-up.js
@@ -6,8 +6,3 @@ function onBtnScrollUp() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-// function topFunction() {
-//     document.body.scrollTop = 0; // For Safari
-//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
